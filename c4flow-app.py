3e31b9e5ef1228a6c96f891b1398c53810470d74
Edriano Souza <79071899+souza-eab@users.noreply.github.com>
2026-03-23 01:10:42 -0300

    DW c4flow-app.py

--- c4flow-app.py
+++ c4flow-app.py
@@ -754,23 +754,28 @@ with tabs[3]:
         if df_proj.empty:
             st.info("Selecione um estado e projeto para visualizar a análise")
         else:
-            col_m1, col_m3, col_m4, col_m5, col_m6 = st.columns(5)
+            col_m1, col_m2, col_m3, col_m4, col_m5 = st.columns(5)
+
             with col_m1:
-                if 'Mean' in df_proj.columns:
+                if 'Mean' in df_proj.columns and 'IC_Mais' in df_proj.columns:
                     st.metric("Média VCUs", f"{df_proj['Mean'].iloc[0]:,.0f} ± {df_proj['IC_Mais'].iloc[0] - df_proj['Mean'].iloc[0]:,.0f}")
-            with col_m3:
+
+            with col_m2:
                 if 'protocol' in df_proj.columns:
                     st.metric("Protocolo", df_proj['protocol'].iloc[0], delta_color="off")
-            with col_m4:
+
+            with col_m3:
                 if 'vcsProjectStatus' in df_proj.columns:
                     st.metric("Status", df_proj['vcsProjectStatus'].iloc[0], delta_color="off")
-            with col_m5:
+
+            with col_m4:
                 if 'resourceIdentifier' in df_proj.columns:
                     st.metric("ID", df_proj['resourceIdentifier'].iloc[0], delta_color="off")
-            with col_m6:
-                if 'vcsAFOLUActivity' in df_proj.columns:
-                    st.metric("ID", df_proj['vcsAFOLUActivity'].iloc[0], delta_color="off")
 
+            with col_m5:
+                if 'vcsAFOLUActivity' in df_proj.columns:
+                    st.metric("Atividade", df_proj['vcsAFOLUActivity'].iloc[0], delta_color="off")
+                                
             st.divider()
 
             col_graf1, col_graf2 = st.columns([3, 1.5])
