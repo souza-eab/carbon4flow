0beba761417a9592369c442ac4d1b43169f36691
Edriano Souza <79071899+souza-eab@users.noreply.github.com>
2026-03-23 01:05:46 -0300

    Update c4flow-app.py

--- c4flow-app.py
+++ c4flow-app.py
@@ -754,7 +754,7 @@ with tabs[3]:
         if df_proj.empty:
             st.info("Selecione um estado e projeto para visualizar a análise")
         else:
-            col_m1, col_m3, col_m4, col_m5 = st.columns(4)
+            col_m1, col_m3, col_m4, col_m5, col_m6 = st.columns(6)
             with col_m1:
                 if 'Mean' in df_proj.columns:
                     st.metric("Média VCUs", f"{df_proj['Mean'].iloc[0]:,.0f} ± {df_proj['IC_Mais'].iloc[0] - df_proj['Mean'].iloc[0]:,.0f}")
@@ -766,7 +766,10 @@ with tabs[3]:
                     st.metric("Status", df_proj['vcsProjectStatus'].iloc[0], delta_color="off")
             with col_m5:
                 if '' in df_proj.columns:
-                    st.metric("Status", df_proj['vcsProjectStatus'].iloc[0], delta_color="off")
+                    st.metric("ID", df_proj['resourceIdentifier'].iloc[0], delta_color="off")
+            with col_m6:
+                if '' in df_proj.columns:
+                    st.metric("ID", df_proj['vcsAFOLUActivity'].iloc[0], delta_color="off")
 
             st.divider()
 
