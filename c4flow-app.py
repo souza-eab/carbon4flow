4acdb0a7ced102c62f4065b21572591b71e14ad5
Edriano Souza <79071899+souza-eab@users.noreply.github.com>
2026-04-23 14:36:38 -0300

    Add logo IPAM to Streamlit app

--- c4flow-app.py
+++ c4flow-app.py
@@ -402,6 +402,14 @@ st.set_page_config(
     initial_sidebar_state="auto"
 )
 
+#st.logo(image, *, size="medium", link=None, icon_image=None)
+# img/ipam_logo.png
+st.logo(
+    "https://github.com/souza-eab/carbon4flow/blob/main/img/ipam_logo.png",
+    size = "small"
+    link="https://ipam.org.br",
+    icon_image="https://github.com/souza-eab/carbon4flow/blob/main/img/QR_CODE_C4FLOW.png"
+)
 # =====================================
 # FUNÇÕES AUXILIARES
 # =====================================
